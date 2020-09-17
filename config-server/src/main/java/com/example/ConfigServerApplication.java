b116c3c21dc42ec2e56cd4b03f065e8a755b01a0
lhn <lhn0128@163.com>
2020-09-17 11:17:19 +0800

    spring cloud test

--- config-server/src/main/java/com/example/ConfigServerApplication.java
+++ config-server/src/main/java/com/example/ConfigServerApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+  *  @Author Liu Haonan
+  *  @Date 2020/9/16 19:04
+  *  @Description 微服务配置中心
+  */
+
 @EnableConfigServer//开启配置中心服务
 @SpringBootApplication
 public class ConfigServerApplication {
--- user-service/src/main/java/com/example/controller/UserController.java
+++ user-service/src/main/java/com/example/controller/UserController.java
@@ -5,20 +5,21 @@ import com.example.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RefreshScope//配置自动更新配置文件
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService userService;
-//    @Value("${testBus.name}")
-//    private String name;
+    @Value("${testBus.name}")
+    private String name;
 
 
 
@@ -34,7 +35,7 @@ public class UserController {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }*/
-//        System.out.println("配置文件中的testBus的属性值为："+name);
+        System.out.println("配置文件中的testBus的属性值为："+name);
         return userService.queryById(id);
     }
 }
